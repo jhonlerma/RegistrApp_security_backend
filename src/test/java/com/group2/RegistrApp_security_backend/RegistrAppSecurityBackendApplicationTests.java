807f4b9a988aa7e46bf7c146c40cc9d9efd2e28d
jhonlerma <john.enfermeria3645@gmail.com>
2022-09-05 16:41:36 -0500

    initial commit

---
+++ src/test/java/com/group2/RegistrApp_security_backend/RegistrAppSecurityBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group2.RegistrApp_security_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrAppSecurityBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
