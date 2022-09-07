3c46922ba228fbc2a545b89e1263594f82b4aae0
jhonlerma <john.enfermeria3645@gmail.com>
2022-09-06 22:18:19 -0500

    se llega hasta la clase del 30 XD

---
+++ src/main/java/com/group2/RegistrApp_security_backend/db/entities/Role.java
@@ -0,0 +1,25 @@
+package com.group2.RegistrApp_security_backend.db.entities;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Document()
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Role {
+
+    @Id
+    private String _id;
+    private String name;
+    private String role;
+    
+}
--- src/main/java/com/group2/RegistrApp_security_backend/services/implementation/RoleServiceImplementation.java
+++ src/main/java/com/group2/RegistrApp_security_backend/services/implementation/RoleServiceImplementation.java
@@ -1,19 +1,32 @@
 package com.group2.RegistrApp_security_backend.services.implementation;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.group2.RegistrApp_security_backend.db.entities.Role;
+import com.group2.RegistrApp_security_backend.db.repositories.RoleRepository;
 import com.group2.RegistrApp_security_backend.dtos.response.RoleResponseDto;
 import com.group2.RegistrApp_security_backend.services.RoleServiceInterface;
 
 @Service
 public class RoleServiceImplementation implements RoleServiceInterface {
 
+    @Autowired
+    private RoleRepository repository;
+    
     @Override
     public List<RoleResponseDto> getAllRoles() {
-        // TODO Auto-generated method stub
-        return null;
+
+        List<Role> roles = this.repository.findAll();
+        return roles.stream().map(x -> {
+            return RoleResponseDto.builder()
+            .name(x.getName())
+            .descirption(x.getName())
+            .build();
+        }).collect(Collectors.toList());
     }
     
 }
